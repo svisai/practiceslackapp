1488d980f1ef1126ae827cab0a7b8be9f044a309
Samidha Visai <svisai@umich.edu>
2016-10-08 22:07:36 -0400

    fix:

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
-from bottle import run, post, get
+from bottle import run, post, get, os
 @post('/hello')
 def hello():
     return 'Hi World!'
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=5000)
+    port = os.environ.get('PORT', 5000)
+    run(host='0.0.0.0', port=port)
